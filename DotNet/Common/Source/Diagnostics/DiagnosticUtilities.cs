841b44ddbb700be537e1ebdd170cef7ef952fe96
lixinxu <lixinxu@hotmail.com>
2016-05-25 13:59:53 -0700

    simplify desing by moving logging common code to utility

--- DotNet/Common/Source/Diagnostics/DiagnosticUtilities.cs
+++ DotNet/Common/Source/Diagnostics/DiagnosticUtilities.cs
@@ -7,6 +7,7 @@
 namespace CloudLibrary.Common.Diagnostics
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Text;
 
@@ -16,13 +17,53 @@ namespace CloudLibrary.Common.Diagnostics
     public static class DiagnosticUtilities
     {
         /// <summary>
-        /// GEt method information
+        /// Add stack trace information into message builder
         /// </summary>
-        /// <param name="frame">stack frame</param>
-        /// <returns>method information</returns>
-        public static string GetMethodInformation(StackFrame frame)
+        /// <param name="builder">string builder</param>
+        /// <param name="stackTrace">stack trace</param>
+        /// <param name="title">stack trace title</param>
+        /// <param name="fileLabel">file label</param>
+        /// <param name="lineLable">line label</param>
+        /// <param name="columnLabel">column label</param>
+        public static void AppendStackTraceInformation(
+            StringBuilder builder, 
+            StackTrace stackTrace, 
+            string title = "Stack trace",
+            string fileLabel = "File",
+            string lineLable = "Line",
+            string columnLabel = "Column")
         {
-            var builder = new StringBuilder();
+            if (stackTrace != null)
+            {
+                var frames = stackTrace.GetFrames();
+                if (!frames.IsNullOrEmpty())
+                {
+                    builder.AppendLine($"{title}:");
+                    foreach (var frame in frames)
+                    {
+                        builder.Append('\t');
+                        DiagnosticUtilities.AppendMethodInformation(builder, frame, fileLabel, lineLable, columnLabel);
+                        builder.AppendLine();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add method information to message builder
+        /// </summary>
+        /// <param name="builder">string builder</param>
+        /// <param name="frame">stack frame</param>
+        /// <param name="fileLabel">file label</param>
+        /// <param name="lineLable">line label</param>
+        /// <param name="columnLabel">column label</param>
+        public static void AppendMethodInformation(
+            StringBuilder builder, 
+            StackFrame frame,
+            string fileLabel,
+            string lineLable,
+            string columnLabel)
+        {
             try
             {
                 var methodInformation = frame.GetMethod();
@@ -43,17 +84,61 @@ namespace CloudLibrary.Common.Diagnostics
                     }
                 }
 
-                builder.Append(") ");
-                builder.AppendFormat("File: \"{0}\" ", frame.GetFileName());
-                builder.AppendFormat("Line: \"{0}\" ", frame.GetFileLineNumber());
-                builder.AppendFormat("Column: \"{0}\" ", frame.GetFileColumnNumber());
+                builder.Append(")");
+                var filename = frame.GetFileName();
+                if (!string.IsNullOrEmpty(filename))
+                {
+                    builder.AppendFormat(" {0}:\"{1}\"", fileLabel, filename);
+
+                    var lineNumber = frame.GetFileLineNumber();
+                    if (lineNumber > 0)
+                    {
+                        builder.AppendFormat(" {0}:{1}", lineLable, lineNumber);
+                    }
+
+                    var columnNumber = frame.GetFileColumnNumber();
+                    if (columnNumber > 0)
+                    {
+                        builder.AppendFormat(" {0}:{1}", columnLabel, columnNumber);
+                    }
+                }
             }
             catch (Exception exception)
             {
                 builder.AppendLine(exception.ToString());
             }
+        }
 
-            return builder.ToString();
+        /// <summary>
+        /// Add runtime information
+        /// </summary>
+        /// <param name="builder">message builder</param>
+        /// <param name="runtimeInformation">runtime information</param>
+        /// <param name="title">title for runtime information</param>
+        public static void AddRuntimeInformation(
+            StringBuilder builder, 
+            IReadOnlyDictionary<string, object> runtimeInformation,
+            string title = "Runtime Information")
+        {
+            if (!runtimeInformation.IsReadOnlyNullOrEmpty())
+            {
+                builder.AppendFormat("{0}:", title);
+                builder.AppendLine();
+                foreach (var pair in runtimeInformation)
+                {
+                    string value;
+                    try
+                    {
+                        value = pair.Value.ToString();
+                    }
+                    catch (Exception exception)
+                    {
+                        value = exception.ToString();
+                    }
+
+                    builder.AppendLine($"\t{pair.Key}: {value}");
+                }
+            }
         }
     }
 }
