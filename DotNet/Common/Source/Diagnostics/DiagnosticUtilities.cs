6a6c5dd5067a48e414266c383288ccdfd1e2da0c
lixinxu <lixin@hotmail.com>
2016-05-25 03:16:11 -0700

    add trace logger

---
+++ DotNet/Common/Source/Diagnostics/DiagnosticUtilities.cs
@@ -0,0 +1,59 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="DiagnosticUtilities.cs" company="CloudLibrary">
+//    Copyright (c) CloudLibrary 2016.  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace CloudLibrary.Common.Diagnostics
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+
+    /// <summary>
+    /// Utilities for diagnostic
+    /// </summary>
+    public static class DiagnosticUtilities
+    {
+        /// <summary>
+        /// GEt method information
+        /// </summary>
+        /// <param name="frame">stack frame</param>
+        /// <returns>method information</returns>
+        public static string GetMethodInformation(StackFrame frame)
+        {
+            var builder = new StringBuilder();
+            try
+            {
+                var methodInformation = frame.GetMethod();
+                builder.AppendFormat("{0}(", methodInformation.Name);
+                var parameters = methodInformation.GetParameters();
+                if (!parameters.IsNullOrEmpty())
+                {
+                    var first = false;
+                    foreach (var parameter in parameters)
+                    {
+                        if (!first)
+                        {
+                            builder.Append(", ");
+                            first = false;
+                        }
+
+                        builder.AppendFormat("{0} {1}", parameter.ParameterType.Name, parameter.Name);
+                    }
+                }
+
+                builder.Append(") ");
+                builder.AppendFormat("File: \"{0}\" ", frame.GetFileName());
+                builder.AppendFormat("Line: \"{0}\" ", frame.GetFileLineNumber());
+                builder.AppendFormat("Column: \"{0}\" ", frame.GetFileColumnNumber());
+            }
+            catch (Exception exception)
+            {
+                builder.AppendLine(exception.ToString());
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ DotNet/Common/Source/Diagnostics/TraceLogger.cs
@@ -0,0 +1,86 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="TraceLogger.cs" company="CloudLibrary">
+//    Copyright (c) CloudLibrary 2016.  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace CloudLibrary.Common.Diagnostics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+
+    /// <summary>
+    /// ILog trance implementation
+    /// </summary>
+    public class TraceLogger : ILog
+    {
+        /// <summary>
+        /// Trace prefix
+        /// </summary>
+        public const string TracePrefix = "CloudLibrary:";
+
+        /// <summary>
+        /// Write event log
+        /// </summary>
+        /// <param name="eventSource">event source name</param>
+        /// <param name="eventId">event id</param>
+        /// <param name="eventLevel">event level</param>
+        /// <param name="message">detail event information</param>
+        /// <param name="eventTime">the time the event happened</param>
+        /// <param name="stackTrace">stack trace</param>
+        /// <param name="runtimeInformation">additional runtime information</param>
+        public void Write(
+            string eventSource,
+            int eventId,
+            EventLevel eventLevel,
+            string message,
+            DateTime eventTime,
+            StackTrace stackTrace,
+            IReadOnlyDictionary<string, object> runtimeInformation)
+        {
+            var builder = new StringBuilder(TracePrefix);
+            builder.AppendLine($"[Source:{eventSource}][id:{eventId}][Level:{eventLevel}][Time:{eventTime.ToString()}");
+            if (!string.IsNullOrEmpty(message))
+            {
+                builder.AppendLine("Message:");
+                builder.AppendLine($"\t{message}");
+            }
+
+            if (stackTrace != null)
+            {
+                var frames = stackTrace.GetFrames();
+                if (!frames.IsNullOrEmpty())
+                {
+                    builder.AppendLine("Stack trace:");
+                    foreach (var frame in frames)
+                    {
+                        builder.AppendLine($"\t{DiagnosticUtilities.GetMethodInformation(frame)}");
+                    }
+                }
+            }
+
+            if (!runtimeInformation.IsReadOnlyNullOrEmpty())
+            {
+                builder.AppendLine("Runtime Information:");
+                foreach (var pair in runtimeInformation)
+                {
+                    string value;
+                    try
+                    {
+                        value = pair.Value.ToString();
+                    }
+                    catch (Exception exception)
+                    {
+                        value = exception.ToString();
+                    }
+
+                    builder.AppendLine($"\t{pair.Key}: {value}");
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
