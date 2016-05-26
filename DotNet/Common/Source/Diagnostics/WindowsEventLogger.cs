fb93f07e65797924f60210911e9501fbb671829d
Octavic <yifeixu@live.com>
2016-05-25 22:55:55 -0700

    Added WindowsEventLogger
    Added WindowsEventLogger, a class that implements ILog as well as the
    unit test for the class

---
+++ DotNet/Common/Source/Diagnostics/WindowsEventLogger.cs
@@ -0,0 +1,77 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="WindowsEventLogger.cs" company="CloudLibrary">
+//    Copyright (c) CloudLibrary 2016.  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace CloudLibrary.Common.Diagnostics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+
+    /// <summary>
+    /// ILog Windows Event implementation
+    /// </summary>
+    public class WindowsEventLogger : ILog
+    {
+        /// <summary>
+        /// Write the Windows Event Log
+        /// </summary>
+        /// <param name="eventSource">event source name</param>
+        /// <param name="eventId">event id</param>
+        /// <param name="eventLevel">event level</param>
+        /// <param name="message">detail event information</param>
+        /// <param name="eventTime">the time the event happened</param>
+        /// <param name="stackTrace">stack trace</param>
+        /// <param name="runtimeInformation">additional runtime information</param>
+        public void Write(
+        string eventSource,
+        int eventId,
+        EventLevel eventLevel,
+        string message,
+        DateTime eventTime,
+        StackTrace stackTrace,
+        IReadOnlyDictionary<string, object> runtimeInformation)
+        {
+            // Choose the corresponding Windows EventLogEntryType with the given EventLevel
+            var resultEventLogEntryType = EventLogEntryType.Error;
+            switch (eventLevel)
+            {
+                case EventLevel.Debug:
+                case EventLevel.Information:
+                    resultEventLogEntryType = EventLogEntryType.Information;
+                    break;
+                case EventLevel.Warning:
+                    resultEventLogEntryType = EventLogEntryType.Warning;
+                    break;
+                default:
+                    break;
+            }
+
+            // Build the message
+            var builder = new StringBuilder();
+            builder.AppendLine($"Time:{ eventTime.ToString()}");
+            if (!string.IsNullOrEmpty(message))
+            {
+                builder.AppendLine("Message:");
+                builder.AppendLine($"\t{message}");
+            }
+
+            // Append the stack trace and additional information to the builder
+            DiagnosticUtilities.AppendStackTraceInformation(builder, stackTrace);
+            DiagnosticUtilities.AddRuntimeInformation(builder, runtimeInformation);
+
+            // Try to write the log if the program has the access to do so
+            try
+            {
+                EventLog.WriteEntry(eventSource, builder.ToString(), resultEventLogEntryType, eventId);
+            }
+            catch (Exception e)
+            {
+                //TODO: Report error when the access is denied, or when the source doesnot exist
+            }
+        }
+    }
+}
---
+++ DotNet/Common/UnitTest/DiagnosticsTest/WindowsEventLoggerUnitTest.cs
@@ -0,0 +1,103 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="WindowsEventLoggerUnitTest.cs" company="CloudLibrary">
+//    Copyright (c) CloudLibrary 2016.  All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace CloudLibrary.Common.UnitTest.DiagnosticsTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Common.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Shared.CommonUnitTestHelper;
+    using Shared.UnitTestHelper;
+
+    /// <summary>
+    /// Windows Event Logger unit test
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class WindowsEventLoggerUnitTest
+    {
+        /// <summary>
+        /// Test Windows Event Logger
+        /// </summary>
+        [TestMethod]
+        public void WindowsEventLogger_UnitTest()
+        {
+            var testCount = 3;
+            var random = new Random();
+
+            var logger = new WindowsEventLogger();
+            var stackTrace = new StackTrace(true);
+            var runtimeInformation = new Dictionary<string, object>();
+
+            // Run through the test
+            for (var testId = 0; testId < testCount; testId++)
+            {
+                var eventId = Math.Abs(random.Next()) % 256;
+                var allEventLevels = EnumHelper.GetAllPossibleValues<EventLevel>();
+
+                // Test all event levels
+                foreach (var eventLevel in allEventLevels)
+                {
+                    var eventSource = TestHelper.CreateUniqueName("SourceName{0}");
+                    var message = TestHelper.CreateUniqueName("EventMessage{0}");
+                    runtimeInformation.Clear();
+                    var runtimeItemCount = random.Next(10) + 1;
+                    for (var i = 0; i < runtimeItemCount; i++)
+                    {
+                        runtimeInformation.Add(
+                            TestHelper.CreateUniqueName("RuntimeKey{0}"),
+                            TestHelper.CreateUniqueName("RuntimeValue{0}"));
+                    }
+
+                    // Write the log
+                    logger.Write(
+                        eventSource,
+                        eventId,
+                        eventLevel,
+                        message,
+                        DateTime.UtcNow,
+                        stackTrace,
+                        runtimeInformation);
+
+                    // Get all the logs from this machine
+                    var eventLogs = EventLog.GetEventLogs();
+
+                    // Find the right log
+                    var eventLogName = EventLog.LogNameFromSourceName(eventSource, Environment.MachineName);
+                    if (string.IsNullOrEmpty(eventLogName))
+                    {
+                        Assert.Fail();
+                    }
+                    var eventLog = new EventLog(eventLogName, Environment.MachineName, eventSource);
+                    // Get the current entry from newest to oldest
+                    for (int i = eventLog.Entries.Count - 1; i > 0; i--)
+                    {
+                        // Get the current entry from newest to oldest
+                        var currentEntry = eventLog.Entries[i];
+                        if (currentEntry.Source != eventSource)
+                        {
+                            continue;
+                        }
+                        // Found the correct entry, verity the contents
+                        var logMessage = currentEntry.Message;
+                        Assert.IsTrue(logMessage.Contains(message));
+                        foreach (var pair in runtimeInformation)
+                        {
+                            Assert.IsTrue(logMessage.Contains(pair.Key));
+                            Assert.IsTrue(logMessage.Contains(pair.Value as string));
+                        }
+                        return;
+                    }
+                    Assert.Fail();
+                }
+            }
+        }
+    }
+}
