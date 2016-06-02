2b686c3075cad1b1943392b7f14a6edaaeafce34
Octavic <yifeixu@live.com>
2016-06-01 19:26:54 -0700

    Changed the event logger unit test
    Changed the event logger unit test to be more efficient

--- DotNet/Common/UnitTest/DiagnosticsTest/WindowsEventLoggerUnitTest.cs
+++ DotNet/Common/UnitTest/DiagnosticsTest/WindowsEventLoggerUnitTest.cs
@@ -6,15 +6,13 @@
 
 namespace CloudLibrary.Common.UnitTest.DiagnosticsTest
 {
+    using Common.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Shared.UnitTestHelper;
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Diagnostics.CodeAnalysis;
-
-    using Common.Diagnostics;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Shared.CommonUnitTestHelper;
-    using Shared.UnitTestHelper;
 
     /// <summary>
     /// Windows Event Logger unit test
@@ -29,17 +27,19 @@ namespace CloudLibrary.Common.UnitTest.DiagnosticsTest
         [TestMethod]
         public void WindowsEventLogger_UnitTest()
         {
-            var testCount = 3;
+            var testCount = 1;
             var random = new Random();
 
             var logger = new WindowsEventLogger();
             var stackTrace = new StackTrace(true);
-            var runtimeInformation = new Dictionary<string, object>();
 
-            // Run through the test
+            // Keep track of all the data for verifying later
+            var testInformationList = new Dictionary<string, Tuple<string, Dictionary<string, object>>>();
+
+            // Run through all the tests
             for (var testId = 0; testId < testCount; testId++)
             {
-                var eventId = Math.Abs(random.Next()) % 256;
+                var eventId = Math.Abs(random.Next(256));
                 var allEventLevels = EnumHelper.GetAllPossibleValues<EventLevel>();
 
                 // Test all event levels
@@ -47,7 +47,7 @@ namespace CloudLibrary.Common.UnitTest.DiagnosticsTest
                 {
                     var eventSource = TestHelper.CreateUniqueName("SourceName{0}");
                     var message = TestHelper.CreateUniqueName("EventMessage{0}");
-                    runtimeInformation.Clear();
+                    var runtimeInformation = new Dictionary<string, object>();
                     var runtimeItemCount = random.Next(10) + 1;
                     for (var i = 0; i < runtimeItemCount; i++)
                     {
@@ -66,38 +66,56 @@ namespace CloudLibrary.Common.UnitTest.DiagnosticsTest
                         stackTrace,
                         runtimeInformation);
 
-                    // Get all the logs from this machine
-                    var eventLogs = EventLog.GetEventLogs();
-
-                    // Find the right log
-                    var eventLogName = EventLog.LogNameFromSourceName(eventSource, Environment.MachineName);
-                    if (string.IsNullOrEmpty(eventLogName))
-                    {
-                        Assert.Fail();
-                    }
-                    var eventLog = new EventLog(eventLogName, Environment.MachineName, eventSource);
-                    // Get the current entry from newest to oldest
-                    for (int i = eventLog.Entries.Count - 1; i > 0; i--)
-                    {
-                        // Get the current entry from newest to oldest
-                        var currentEntry = eventLog.Entries[i];
-                        if (currentEntry.Source != eventSource)
-                        {
-                            continue;
-                        }
-                        // Found the correct entry, verity the contents
-                        var logMessage = currentEntry.Message;
-                        Assert.IsTrue(logMessage.Contains(message));
-                        foreach (var pair in runtimeInformation)
-                        {
-                            Assert.IsTrue(logMessage.Contains(pair.Key));
-                            Assert.IsTrue(logMessage.Contains(pair.Value as string));
-                        }
-                        return;
-                    }
-                    Assert.Fail();
+                    // Add the information to the lists
+                    testInformationList.Add(eventSource, new Tuple<string, Dictionary<string, object>>(message, runtimeInformation));
                 }
             }
+
+            // Add all the logs to a log stack
+            var totalEventLogs = EventLog.GetEventLogs(Environment.MachineName);
+            EventLogEntryCollection applicationEventLogs = null;
+            foreach (var log in totalEventLogs)
+            {
+                if (log.Log == "Application")
+                {
+                    applicationEventLogs = log.Entries;
+                }
+            }
+
+            Assert.IsNotNull(applicationEventLogs);
+
+            // Go through all the logs and try to match the source
+            for (int i = applicationEventLogs.Count - 1; i >= 0; i--)
+            {
+                var currentEntry = applicationEventLogs[i];
+                var currentEntryEventSource = currentEntry.Source;
+                Tuple<string, Dictionary<string, object>> resultTestInformation;
+                if (!testInformationList.TryGetValue(currentEntryEventSource, out resultTestInformation))
+                {
+                    continue;
+                }
+
+                // Found the correct entry, verity the contents
+                var logMessage = currentEntry.Message;
+
+                // Get the messsage and runtime information
+                var currentMessage = resultTestInformation.Item1;
+                var currentRuntimeInformation = resultTestInformation.Item2;
+
+                // Remove the entry from the dictionary
+                testInformationList.Remove(currentEntryEventSource);
+
+                // Verify the test information
+                Assert.IsTrue(logMessage.Contains(currentMessage));
+                foreach (var pair in currentRuntimeInformation)
+                {
+                    Assert.IsTrue(logMessage.Contains(pair.Key));
+                    Assert.IsTrue(logMessage.Contains(pair.Value as string));
+                }
+            }
+
+            // If there are still things left in the lists, that means there is a log that was NOT written to the windwos event logs
+            Assert.IsTrue(testInformationList.IsNullOrEmpty());
         }
     }
 }
